33673cc518dbc0665ebfb94472b4bd4f005bbba1
Filip Filipovic <filipfilipo67@gmail.com>
2023-06-08 11:55:53 +0200

    Finish the Project,adding Media Querris

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+
 function setPokemonBackgroundColor(species) {
   const colorMap = {
     fire: 'red',
@@ -22,7 +23,10 @@ function setPokemonBackgroundColor(species) {
     dragon: 'rgb(83,164,207)',
   };
 
-  return colorMap[species] || 'lightgray';
+  const types = species.split(', ');
+  const firstType = types[0];
+
+  return colorMap[firstType] || 'lightgray';
 }
 
 
@@ -32,59 +36,6 @@ searchInput.addEventListener('keypress', function (event) {
     performSearch();
   }
 });
-
-
-async function performSearch() {
-  let searchInput = document.getElementById('searchPokemon').value;
-  let searchTerm = searchInput.toLowerCase();
-
-  let searchResults = [];
-
-  allPokemon.forEach(pokemon => {
-    let pokemonName = pokemon['name'].toLowerCase();
-    if (pokemonName.includes(searchTerm)) {
-      searchResults.push(pokemon);
-    }
-  });
-
-  if (searchResults.length === 0) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
-    let response = await fetch(url);
-
-    if (response.ok) {
-      let pokemonData = await response.json();
-      searchResults.push(pokemonData);
-    } else {
-      console.log('Fehler beim Laden der Pokemon-Daten');
-      return;
-    }
-  }
-
-  searchResults.forEach(pokemon => {
-    let pokemonName = pokemon['name'];
-    let species = pokemon['types'][0]['type']['name'];
-    let pokemonImage = pokemon['sprites']['other']['dream_world']['front_default'];
-    let weight = pokemon['weight'];
-
-    showSearchedPokemon(pokemonName, species, pokemonImage);
-  });
-}
-
-
-
-
-function showSearchedPokemon(name, species, image) {
-  let searchedPokemon = document.getElementById('searchedPokemon');
-  searchedPokemon.style.display = 'block';
-  searchedPokemon.innerHTML = '';
-
-  searchedPokemon.innerHTML = `
-  <div class="currentPokemonContainer">
-      <img src="${image}" alt="">
-      <h1>${name}</h1>
-      <div>${species}</div>  
-  </div>`;
-}
 
 
 async function performSearch() {
@@ -110,23 +61,59 @@ async function performSearch() {
     let species = pokemon['types'][0]['type']['name'];
     let pokemonImage = pokemon['sprites']['other']['dream_world']['front_default'];
     let weight = pokemon['weight'];
-    showSearchedPokemon(pokemonName, species, pokemonImage);
+
+    showSearchedPokemon(pokemonName, species, pokemonImage, weight);
   });
 }
 
 
-async function loadAllPokemon() {
-  for (let i = 0; i < 50; i++) {
+function showSearchedPokemon(name, species, image, weight) {
+  let searchedPokemon = document.getElementById('clickedPokemon');
+  let backgroundColor = setPokemonBackgroundColor(species);
+  clickedPokemon.style.display = 'block';
+  clickedPokemon.innerHTML = '';
+
+  clickedPokemon.innerHTML += `
+  <div class="currentPokemonContainer" onclick="removeSearcedPokemon()" style="background-color: ${backgroundColor}">
+      <img src=" ${image}" alt="">
+      <h1>${capitalizeFirstLetter(name)}</h1>
+      <h3>Type: ${capitalizeFirstLetter(species)}</h3>  
+      <h3>Weight: ${weight}</h3>  
+  </div>`;
+}
+
+function removeSearcedPokemon() {
+  document.getElementById('clickedPokemon').style.display = 'none';
+  document.querySelector('.currentPokemonContainer').style.display = 'none';
+}
+
+
+window.addEventListener('scroll', function () {
+  if ((document.documentElement.scrollHeight - window.innerHeight) === Math.ceil(window.scrollY)) {
+    const start = allPokemon.length;
+    const end = Math.min(start + 15, 800);
+    loadAllPokemon(start, end);
+  }
+});
+
+function loadAllPokemonOnLoad() {
+  const start = 0;
+  const end = Math.min(start + 50, 800);
+  loadAllPokemon(start, end);
+}
+
+async function loadAllPokemon(start, end) {
+  for (let i = start; i < end; i++) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i + 1}/`;
     let response = await fetch(url);
     pokemonAsJSON = await response.json();
-    allPokemon.push(pokemonAsJSON);
+    allPokemon.push(await pokemonAsJSON);
     currentPokemon.push(allPokemon[i]['name']);
-    console.log(allPokemon[0]);
-
 
     let pokemonName = allPokemon[i]['name'];
-    let species = allPokemon[i]['types'][0]['type']['name'];
+    let type1 = allPokemon[i]['types'][0]['type']['name'];
+    let type2 = allPokemon[i]['types'][1] ? capitalizeFirstLetter(allPokemon[i]['types'][1]['type']['name']) : null;
+    let species = type2 ? `${type1}, ${type2}` : type1;
     let pokemonImage = allPokemon[i]['sprites']['other']['dream_world']['front_default'];
     let pokemonWeight = allPokemon[i]['weight'];
     let order = allPokemon[i]['moves']['order'];
@@ -140,10 +127,10 @@ function showPokemon(name, species, image, pokemonWeight) {
   let backgroundColor = setPokemonBackgroundColor(species);
 
   pokemonContainer.innerHTML += `
-       <div class="pokemonCard" onclick="showPokemonData(name, species, image, pokemonWeight) " style="background-color: ${backgroundColor}">
+       <div class="pokemonCard"  onclick="showPokemonData('${name}', '${species}', '${image}', ${pokemonWeight})" " style="background-color: ${backgroundColor}">
             <h2>${capitalizeFirstLetter(name)} </h2>
             <img src="${image}" alt="">
-            <h3>Species: ${capitalizeFirstLetter(species)}</h3>
+            <h3>Type: ${capitalizeFirstLetter(species)}</h3>
             <h3>Weight: ${pokemonWeight} lb</h3>
        </div>     
     `;
@@ -151,9 +138,27 @@ function showPokemon(name, species, image, pokemonWeight) {
 
 function showPokemonData(name, species, image, pokemonWeight) {
   let data = document.getElementById('searchedPokemon');
+  let backgroundColor = setPokemonBackgroundColor(species);
   data.style.display = 'block';
-  data.innerHTML = '';
+  data.innerHTML = ``;
 
-  data.innerHTML = '';
+  data.innerHTML = `
+  <div class="clickedPokemonCard"  onclick="removePokemonCard()" style="background-color: ${backgroundColor}">
+       <h1>${capitalizeFirstLetter(name)} </h1>
+       <img src="${image}" alt="">
+       <h2>Type: ${capitalizeFirstLetter(species)}</h2>
+       <h2>Weight: ${pokemonWeight} lb</h2>
+  </div>     
+`;
 
 }
+
+function removePokemonCard() {
+  document.getElementById('searchedPokemon').data.style.display = 'none';
+  document.querySelector('.clickedPokemonCard').style.display = 'none';
+}
+
+
+
+
+
